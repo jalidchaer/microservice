295ceaf8e6564cb3f9e3971bd52788d0fb93d16a
Jalid <chaerj91@gmail.com>
2022-10-29 20:12:12 -0400

    added config database in user

--- api/components/user/index.js
+++ api/components/user/index.js
@@ -1,5 +1,12 @@
 //const store = require('../../../store/mysql');
-const store = require('../../../store/remote-mysql');
+const config = require('../../../config');
 const ctrl = require('./controller');
+
+let store;
+if (config.remoteDB === true) {
+    store = require('../../../store/remote-mysql');
+} else {
+    store = require('../../../store/mysql');
+}
 
 module.exports = ctrl(store);
